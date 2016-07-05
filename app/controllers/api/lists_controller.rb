f11af5a982b94e2c88b07e402a51e07d1a378921
Sharrell Porter <SharrellPorter@example.com>
2016-07-05 10:48:41 -0400

    added destroy

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -11,4 +11,14 @@ class Api::ListsController < ApiController
       render json: {}, status: 422
     end
   end
+
+  def destroy
+    begin
+      list = List.find(params[:id])
+      list.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end
 end
