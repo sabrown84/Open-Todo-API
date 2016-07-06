6177dc90c770d2842b87fcf2cf8b73924d9fd871
Sharrell Porter <SharrellPorter@example.com>
2016-07-06 09:32:55 -0400

    minor fixes

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,5 +1,8 @@
 class ApiController < ApplicationController
   skip_before_action :verify_autheniticity_token
+
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
+
   private
   def authenticated?
     authenticate_or_request_with_http_basic {|username, password| User.where( username: username, password: password).present? }
